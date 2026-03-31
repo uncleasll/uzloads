25a6a202208ae60465b381f6c030fb3bc2b8db03
Aaron <macbookpro@MacBook-Pro-MacBook.local>
2026-03-31 15:39:52 +0500

    update cors

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://uzloads-frontend.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
